c9b66d90f1f9cb10f3ecaa642bdb8404c3a33a8f
Denis <denelvis@yandex.ru>
2022-04-09 14:07:28 +0300

    init commit, basic structure, added dependencies, modules

---
+++ src/blockchain.rs
@@ -0,0 +1,7 @@
+extern crate serde;
+extern crate serde_json;
+extern crate sha2;
+extern crate time;
+
+use sha2::{Digest, Sha256};
+use std::fmt::Write;
---
+++ src/main.rs
@@ -0,0 +1,3 @@
+mod blockchain;
+
+fn main() {}
